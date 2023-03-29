1a973fa8da815120490cba57a05c939faf2f4974
wararaki <ky7.ott.w@gmail.com>
2023-03-29 22:58:36 +0900

    update

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function main() {
 
-    let img = document.getElementById("svg");
-
     let url = "https://profile-counter.glitch.me/wararaki718/count.svg";
     fetch(url, {mode: "no-cors"})
         .then((response) => response.text())
         .then((svg) => {
-            // const parser = new DOMParser();
-            // const svg = parser.parseFromString(data, "image/svg+xml").querySelector("svg");
-
             console.log(svg);
         });
 }
